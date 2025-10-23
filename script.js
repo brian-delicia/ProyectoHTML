a4e08942a096dbfd322eac59f4525d55230c5f1c
Cruz, Tomas Ezequiel <tomasezequielcruz159@gmail.com>
2025-10-23 17:16:19 -0300

    carrusel actualizacion y cuerpo del inicio

--- script.js
+++ script.js
@@ -1,36 +1,28 @@
-let imagenes =["./imagenes/moto-Carrusel1.png","./imagenes/moto-carrusel2.png","./imagenes/moto-carrusel3.png","./imagenes/moto-carrusel4.png"]
+const carrusel = document.querySelector('.deslizar');
+const imagenes = document.querySelectorAll('.deslizar img');
 
 let indice = 0;
+let anchoImagen = imagenes[0].clientWidth;
+let cantidadImagenes= imagenes.length;
 
 
-mostrar(indice);
-
-// Next/previous controls
-function siguiente(n) {
-  mostrar(indice += n);
+function deslizar(){
+  if (!carrusel) return;
+  carrusel.style.transform = 'translateX(' + (-anchoImagen * indice) + 'px)';
 }
-
-// Thumbnail image controls
-function atras(n) {
-  mostrar(indice += n);
-}
-
-function mostrar(n) {
-  let i;
-  let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("demo");
-  let captionText = document.getElementById("caption");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
+function siguienteImagen() {
+  if (indice >= cantidadImagenes - 1) {
+    indice = 0;
+  } else {
+    indice++;
   }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
+  deslizar();
+}
+function anteriorImagen() {
+  if (indice <= 0) {
+    indice = cantidadImagenes - 1;
+  } else {
+    indice--;
   }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
-  captionText.innerHTML = dots[slideIndex-1].alt;
-}
-}
-
+  deslizar();
+  }
