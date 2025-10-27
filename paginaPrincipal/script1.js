30f397defd3e0ebaa821b470fb5650d2b30fcd51
Cruz, Tomas Ezequiel <tomasezequielcruz159@gmail.com>
2025-10-27 20:02:04 -0300

    Modificaciones en la pagina de inicio

---
+++ paginaPrincipal/script1.js
@@ -0,0 +1,23 @@
+const imagenes =['/imagenes/moto-Carrusel1.png','/imagenes/moto-Carrusel2.png','/imagenes/moto-Carrusel3.png','/imagenes/moto-Carrusel4.png'];
+
+let indice= 0;
+
+function siguiente() {
+    let img = document.getElementById("imgDeCarrusel");
+    img.src = imagenes[indice];
+    indice++;
+    if(indice== imagenes.length){
+        indice=0;
+    }
+}
+function atras(){
+    
+    let img= document.getElementById("imgDeCarrusel");
+    
+        img.src=imagenes[indice-1];
+        indice--;
+        if(indice==0){
+            indice=imagenes.length;
+        }
+}
+
